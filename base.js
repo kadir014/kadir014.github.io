4c96417b74fb3aac3b7494565ac7a70d71ff6042
Kadir Aksoy <46996028+kadir014@users.noreply.github.com>
2023-01-13 14:21:51 +0300

    rework

---
+++ base.js
@@ -0,0 +1,31 @@
+function scrollDown() {
+    document.querySelector(".content").scrollIntoView({behavior: "smooth"});
+}
+
+function scrollEvent(event) {
+    var reveals = document.querySelectorAll(".reveal");
+
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        var elementVisible = 150;
+
+        if (elementTop < windowHeight - elementVisible) {
+            reveals[i].classList.add("active");
+        } else {
+            reveals[i].classList.remove("active");
+        }
+    }
+
+    var navbar = document.querySelector(".navbar")
+    var banner = document.querySelector(".banner")
+    var bannerBottom = banner.getBoundingClientRect().bottom + window.innerHeight/2 + 460;
+
+    if (window.scrollY > bannerBottom) {
+        navbar.classList.add("active")
+    } else {
+        navbar.classList.remove("active")
+    }
+}
+
+addEventListener("scroll", scrollEvent);
