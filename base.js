8794a1bfe8666ab04e76be58882e5d49f999733b
Kadir Aksoy <46996028+kadir014@users.noreply.github.com>
2023-10-26 19:11:38 +0300

    u

--- base.js
+++ base.js
@@ -3,6 +3,10 @@ function scrollDown() {
 }
 
 function scrollEvent(event) {
+    if (document.querySelector(".overlay").classList.contains("is-open")) {
+        return;
+    }
+    
     var reveals = document.querySelectorAll(".reveal");
 
     for (var i = 0; i < reveals.length; i++) {
@@ -42,3 +46,83 @@ if (filename[0] != "index.html" && filename[0] != "") {
     var navbar = document.querySelector(".navbar");
     navbar.classList.add("active");
 }
+
+// https://codepen.io/nxworld/pen/MvJQQy
+$(function() {
+    var $overlay = $('.overlay'),
+        $overlayTrigger = $('.overlay-trigger'),
+        $overlayClose = $('.overlay-close'),
+        openClass = 'is-open';
+  
+    $overlayTrigger.on('click', function(event) {
+        $(".navbar").removeClass("active");
+        $overlay.addClass(openClass);
+        $overlayClose.addClass(openClass);
+
+        document.querySelector("#overlay-image").src = event.target.src;
+
+        let vh = window.innerHeight;
+        let vw = window.innerWidth;
+        let idiv = document.querySelector("#overlay-image-div");
+        let width = document.querySelector("#overlay-image").width;
+        let height = document.querySelector("#overlay-image").height;
+
+        if (vw > vh) {
+            let a = vh / height;
+            let w = width * a;
+
+            idiv.style.width = `${w}px`;
+        }
+        else {
+            let a = vw / width;
+            let h = height * a;
+
+            idiv.style.height = `${h}px`;
+        }
+
+        document.body.style.overflow = 'hidden';
+    });
+  
+    $overlayClose.on('click', function() {
+        $overlayClose.removeClass(openClass);
+        $overlay.removeClass(openClass);
+        $(".navbar").addClass("active");
+
+        let oimg = document.querySelector("#overlay-image");
+        if (oimg.classList.contains("zoomed")) {
+            oimg.classList.remove("zoomed");
+            oimg.style.transform = "scale(1.0)";
+            oimg.style.cursor = "zoom-in";
+        }
+
+        document.body.style.overflow = 'visible';
+    });
+});
+
+document.querySelector("#overlay-image").addEventListener("click", function() {
+    let oimg = document.querySelector("#overlay-image");
+
+    if (oimg.classList.contains("zoomed")) {
+        oimg.classList.remove("zoomed");
+
+        oimg.style.transform = "scale(1.0)";
+        oimg.style.cursor = "zoom-in";
+    }
+    else {
+        oimg.classList.add("zoomed");
+
+        oimg.style.transform = "scale(2.0)";
+        oimg.style.cursor = "zoom-out";
+    }
+})
+
+document.addEventListener("mousemove", function(event) {
+    let oimg = document.querySelector("#overlay-image");
+
+    if (oimg.classList.contains("zoomed")) {
+        const x = (window.innerWidth - event.x - window.innerWidth/2) / 0.8;
+        const y = (window.innerHeight - event.y - window.innerHeight/2) / 0.8;
+
+        oimg.style.setProperty("transform", `translateX(${x}px) translateY(${y}px) scale(2.0)`, "");
+    }
+});
